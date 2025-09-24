60e5226090e5d94540309ca4ada36f47b2ffda85
LAB13_USER ON WS36 <L13X16W36@LabHCI.net>
2025-09-24 16:24:47 +0800

    feature(sitonapp1): setup_settings

--- sitonproject1/sitonproject1/settings.py
+++ sitonproject1/sitonproject1/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'sitonappname1',
+    'sitonapp1',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
